a2c7db03e152e1c08fd3fe4e292640061a8c2c3a
eunhong <dmsghd95@naver.com>
2023-11-29 21:13:32 +0900

    파스칼삼각형_15489_실버4_X : 조합함수 완성

--- baekjoon/combination/파스칼삼각형_15489_실버4.java
+++ baekjoon/combination/파스칼삼각형_15489_실버4.java
@@ -2,12 +2,18 @@ package baekjoon.combination;
 
 public class 파스칼삼각형_15489_실버4 {
 
+    // TODO : factorial 을 구하는 재귀함수를 이용하여 조합함수 만들기
     // 조합 nCr을 구하는 함수, head는 분자, tail은 분모
-    public static int combination(int n, int r, int head, int tail)
+    public static long combination(int n, int r, long head, long tail)
     {
         if(n == r)
         {
             return 1; // nCn은 1
+        }
+        else if(r > n/2)
+        {
+            // 조합의 성질(nCr == nCn-r)을 이용해 계산을 줄인다.
+            return combination(n, n-r, head, tail);
         }
         else if(r == 0)
         {
@@ -21,10 +27,11 @@ public class 파스칼삼각형_15489_실버4 {
     public static void main(String[] args)
     {
         System.out.println(combination(7, 4, 1, 1));
-        System.out.println(combination(0, 0, 1, 1));
+        System.out.println(combination(7, 2, 1, 1));
         System.out.println(combination(1, 0, 1, 1));
         System.out.println(combination(7, 7, 1, 1));
-        System.out.println(combination(7, 0, 1, 1));
+        System.out.println(combination(7, 6, 1, 1));
+        System.out.println(combination(7, 5, 1, 1));
         System.out.println(combination(7, 3, 1, 1));
         System.out.println(combination(7, 1, 1, 1));
     }
